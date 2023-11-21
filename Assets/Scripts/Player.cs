8bafde698fde8149496b9285d5131b9baf7c6fda
WD-ASSEM002\duy.vhk <Dyu>
2023-11-21 15:33:49 +0700

    ! First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+
+
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string playerName;
+    public string discord;
+    public string inGame;
+    public float score;
+    public Team team;
+}
---
+++ Assets/Scripts/Team.cs
@@ -0,0 +1,13 @@
+
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team : MonoBehaviour
+{
+    public string teamName;
+    public List<Player> playersList;
+    public int curToken;
+    public int lossCount;
+    public int winCount;
+}
